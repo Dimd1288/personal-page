cdf2e275de66a52443a161e4dcd52ca05497f70f
Dimd1288 <dimd1288@gmail.com>
2018-03-27 13:28:40 +0300

    убрал пробелы перед знаками препинания

--- JS/five_to_one.js
+++ JS/five_to_one.js
@@ -1,4 +1,4 @@
-var  revString, stringMinusOne, lastLetter, ourString = prompt('Напишите пару слов', "");
+var spaceLess,  revString, stringMinusOne, lastLetter, ourString = prompt('Напишите пару слов', "");
 var lastElement = function (ourString) {
     lastLetter = ourString[ourString.length-1];
     alert(lastLetter);
@@ -13,11 +13,11 @@ minusLastElement(ourString);
 
 var reversString = function(ourString)
 {
-   var arr=ourString.split('');
-    for (var i=ourString.length; i<1; i=i-1)
+   var arr = ourString.split('');
+    for (var i = ourString.length; i<1; i=i-1)
     {arr[i] = ourString[i]}
     arr=arr.reverse('');
-    revString=arr.join('')
+    revString = arr.join('')
     alert(revString)
 }
 reversString(ourString);
@@ -26,7 +26,13 @@ reversString(ourString);
 var spaceDelete = function(ourString)
 {
     
-var spaceLess = ourString.replace(/(^\s+|\s+$| {2,})/g,' ');
+    spaceLess = ourString.replace(/(^\s+|\s+$| {2,})/g,' ');
+    spaceLess = spaceLess.replace(" .", ".");
+    spaceLess = spaceLess.replace(" ,", ",");
+    spaceLess = spaceLess.replace(" ?", "?");
+    spaceLess = spaceLess.replace(" !", "!");
+    spaceLess = spaceLess.replace(" :", ":");
+    spaceLess = spaceLess.replace(" ;", ";");
     
     alert(spaceLess)
 }
