45718eaa036f85d442f0a9773b820b4a8386ea8c
Dimd1288 <dimd1288@gmail.com>
2018-04-02 23:09:59 +0300

    добавлен пункт 3-й лабы: всплывающее окно

--- lab2.js
+++ lab2.js
@@ -28,3 +28,33 @@ send.addEventListener('click', function(){
                                           }
                       )
 
+
+var formLink = document.getElementsByClassName('write_me')[0];
+var blackBack = document.getElementsByClassName('back_in_black')[0];
+var form = document.getElementsByClassName('send_mail')[0];
+
+
+formLink.addEventListener('click', function(){
+     event.preventDefault();
+        
+        form.classList.add('popup_form');
+        blackBack.style.display='block';
+       var closed = document.getElementsByClassName('close')[0];
+      closed.addEventListener('click', function(){
+   form.classList.remove('popup_form');
+   blackBack.style.display='none';
+                  
+                                          }                           
+                           )  
+}
+ )
+                                                      
+                        
+   blackBack.addEventListener('click', function(){
+         blackBack.style.display='none';
+           form.classList.remove('popup_form');    
+                                                  }
+                              )
+
+
+
