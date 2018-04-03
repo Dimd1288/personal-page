1fa7162c5963271d4f5b8e79168a3e2891800c33
Dimd1288 <dimd1288@gmail.com>
2018-04-03 15:04:57 +0300

    добавил "спрятать" по клику,  поменял фотографии и описания у скрытого блока

--- lab2.js
+++ lab2.js
@@ -1,9 +1,12 @@
   var button=document.getElementsByClassName('button')[0];
+  var hidden = document.getElementsByClassName('hidden_button')[0];
+   
       button.addEventListener('click', function(event){
       event.preventDefault();
       var showMe = document.getElementsByClassName('hide')[0];
           showMe.setAttribute('style', 'display:block');
           button.setAttribute('style', 'display:none');
+          hidden.setAttribute('style', 'display: block')
           function appearance(element, speed)
 	  {
 		  setInterval(function(){ element.style.opacity = +element.style.opacity + 0.05;}, speed);
@@ -13,14 +16,24 @@
 	                                      }
 		  
 		  )
-	
+	hidden.addEventListener('click', function(){
+        event.preventDefault();
+        var showMe = document.getElementsByClassName('hide')[0]; 
+        showMe.setAttribute('style', 'display:none');
+        hidden.setAttribute('style', 'display: none');
+        button.setAttribute('style', 'display:block');
+        
+    }
+                           
+                           
+                           
+                           )
   
 
 var send = document.getElementsByClassName('send_button')[0];
 var ness = document.getElementsByClassName('required');
-console.log(ness);
 send.addEventListener('click', function(){
-        event.preventDefault();
+        
             for (var i=0; i<ness.length; i++){
                  ness[i].classList.add('req');
                                              }
