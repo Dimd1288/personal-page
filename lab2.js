8510f9008e4e935315243e555c3b4fa1dcbdffb4
Dimd1288 <dimd1288@gmail.com>
2018-04-05 21:14:53 +0300

    подправил js, подключил jquery

--- lab2.js
+++ lab2.js
@@ -1,73 +1,83 @@
-  var button=document.getElementsByClassName('button')[0];
+  var button = document.getElementsByClassName('button')[0];
   var hidden = document.getElementsByClassName('hidden_button')[0];
-   
-      button.addEventListener('click', function(event){
-      event.preventDefault();
-      var showMe = document.getElementsByClassName('hide')[0];
+
+  button.addEventListener('click', function(event) {
+          event.preventDefault();
+          var showMe = document.getElementsByClassName('hide')[0];
           showMe.setAttribute('style', 'display:block');
           button.setAttribute('style', 'display:none');
           hidden.setAttribute('style', 'display: block')
-          function appearance(element, speed)
-	  {
-		  setInterval(function(){ element.style.opacity = +element.style.opacity + 0.05;}, speed);
-		  
-	  }
-		  appearance(showMe, 100);
-	                                      }
-		  
-		  )
-	hidden.addEventListener('click', function(){
-        event.preventDefault();
-        var showMe = document.getElementsByClassName('hide')[0]; 
-        showMe.setAttribute('style', 'display:none');
-        hidden.setAttribute('style', 'display: none');
-        button.setAttribute('style', 'display:block');
-        
-    }
-                           
-                           
-                           
-                           )
-  
 
-var send = document.getElementsByClassName('send_button')[0];
-var ness = document.getElementsByClassName('required');
-send.addEventListener('click', function(){
-        
-            for (var i=0; i<ness.length; i++){
-                 ness[i].classList.add('req');
-                                             }
-    
-                                          }
-                      )
+          function appearance(element, speed) {
+              if (element.style.opacity < 1) {
+                  setInterval(function() {
+                      element.style.opacity = +element.style.opacity + 0.05;
+                  }, speed);
 
+              }
+          }
+          appearance(showMe, 150);
+      }
 
-var formLink = document.getElementsByClassName('write_me')[0];
-var blackBack = document.getElementsByClassName('back_in_black')[0];
-var form = document.getElementsByClassName('send_mail')[0];
+  )
+  hidden.addEventListener('click', function() {
+          event.preventDefault();
+          var showMe = document.getElementsByClassName('hide')[0];
+          showMe.setAttribute('style', 'display:none');
+          hidden.setAttribute('style', 'display: none');
+          button.setAttribute('style', 'display:block');
 
-
-formLink.addEventListener('click', function(){
-     event.preventDefault();
-        
-        form.classList.add('popup_form');
-        blackBack.style.display='block';
-       var closed = document.getElementsByClassName('close')[0];
-      closed.addEventListener('click', function(){
-   form.classList.remove('popup_form');
-   blackBack.style.display='none';
-                  
-                                          }                           
-                           )  
-}
- )
-                                                      
-                        
-   blackBack.addEventListener('click', function(){
-         blackBack.style.display='none';
-           form.classList.remove('popup_form');    
-                                                  }
-                              )
+      }
 
 
 
+  )
+
+
+  var send = document.getElementsByClassName('send_button')[0];
+  var ness = document.getElementsByClassName('required');
+  send.addEventListener('click', function() {
+
+      for (var i = 0; i < ness.length; i++) {
+          ness[i].classList.add('req');
+      }
+
+  })
+  var myName = document.getElementById('name');
+  myName.addEventListener('click', function() {
+      myName.classList.remove('req');
+  })
+
+  var myMale = document.getElementById('email');
+  myMale.addEventListener('click', function() {
+      myMale.classList.remove('req');
+  })
+
+  var someWords = document.getElementById('someWords');
+  someWords.addEventListener('click', function() {
+      someWords.classList.remove('req');
+  })
+
+  var formLink = document.getElementsByClassName('write_me')[0];
+  var blackBack = document.getElementsByClassName('back_in_black')[0];
+  var form = document.getElementsByClassName('send_mail')[0];
+
+
+  formLink.addEventListener('click', function() {
+      event.preventDefault();
+
+      form.classList.add('popup_form');
+      blackBack.style.display = 'block';
+      var closed = document.getElementsByClassName('close')[0];
+      closed.addEventListener('click', function() {
+          form.classList.remove('popup_form');
+          blackBack.style.display = 'none';
+
+      })
+  })
+
+
+  blackBack.addEventListener('click', function() {
+      blackBack.style.display = 'none';
+      form.classList.remove('popup_form');
+  })
